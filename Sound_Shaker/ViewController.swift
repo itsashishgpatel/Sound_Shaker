6631ea02b50c0dfd4aacb0557e8f006cad95718a
Ashish Patel <itsashishgpatel@yahoo.com>
2019-08-08 07:36:15 -0500

    2

--- Sound_Shaker/ViewController.swift
+++ Sound_Shaker/ViewController.swift
@@ -7,15 +7,90 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    //var audioPlayer: AVAudioPlayer = AVAudioPlayer()
+    
+        var player = AVAudioPlayer()
+  //  var player : AVAudioPlayer!
+    
+    @IBAction func Play(_ sender: Any) {
+        player.play()
+    }
+    
+    
+    @IBAction func Pause(_ sender: Any) {
+        player.pause()
+    }
+    
+    
+    @IBAction func Slide(_ sender: Any) {
+        player.volume = Slide.value
         
     }
+    
+    
+    @IBOutlet weak var Slide: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let audioPath = Bundle.main.path(forResource: "GaliGali", ofType: "mp3")
+        do {
+            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))}
+            
+        catch{
+            print("error")
+        }
+        
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action:
+            #selector(self.swipped(gesture:)))
+        
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        
+       let swipeLeft = UISwipeGestureRecognizer(target: self, action:
+           #selector(self.swipped(gesture:)))
+        
+        
+        
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        
+        
+        
+        }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?)
+    
+    {
+        if event?.subtype == UIEvent.EventSubtype.motionShake {
+            print(" Device was shaken")
+        }
+        
+    }
+    
+    
+    @objc func swipped(gesture: UIGestureRecognizer){
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            if swipeGesture.direction == UISwipeGestureRecognizer.Direction.right {
+                print(" Right Swipe")
+            }
+            else {
+                print("Left")
+            }
+            }
+        }
+    }
+    
+        
 
 
-}
+
+
 
